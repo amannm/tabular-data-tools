7386ad9ae3f114d6471721fcbf58641fcbd6cb90
Amann Malik <amannmalik@gmail.com>
2018-05-19 23:25:27 -0500

    checkpoint

--- src/main/java/com/amannmalik/tabulardatatools/calcite/S3SelectEnumerator.java
+++ src/main/java/com/amannmalik/tabulardatatools/calcite/S3SelectEnumerator.java
@@ -5,26 +5,23 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.AmazonS3URI;
 import com.amazonaws.services.s3.model.*;
 import com.univocity.parsers.csv.CsvFormat;
+import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
-import org.apache.calcite.linq4j.AbstractEnumerable;
 import org.apache.calcite.linq4j.Enumerator;
 
+import java.io.InputStream;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
 
-public class S3SelectQuery extends AbstractEnumerable<Object[]> {
+public class S3SelectEnumerator implements Enumerator<Object[]> {
 
-    private final URI location;
-    private final String query;
 
-    public S3SelectQuery(URI location, String query) {
-        this.location = location;
-        this.query = query;
-    }
+    private String[] currentLine;
 
-    @Override
-    public Enumerator<Object[]> enumerator() {
-        SelectObjectContentResult result = executeQuery();
-        SelectRecordsInputStream recordsInputStream = result.getPayload().getRecordsInputStream();
+    private final CsvParser csvParser;
+
+    public S3SelectEnumerator(URI location, String query) {
         CsvFormat csvFormat = new CsvFormat();
         csvFormat.setDelimiter(',');
         csvFormat.setLineSeparator("\n");
@@ -33,11 +30,13 @@ public class S3SelectQuery extends AbstractEnumerable<Object[]> {
         CsvParserSettings parserSettings = new CsvParserSettings();
         parserSettings.setHeaderExtractionEnabled(false);
         parserSettings.setFormat(csvFormat);
-        return new S3SelectResultSetEnumerator(recordsInputStream, parserSettings);
+        csvParser = new CsvParser(parserSettings);
+        InputStream inputStream = executeQuery(location, query);
+        csvParser.beginParsing(inputStream, StandardCharsets.UTF_8);
+
     }
 
-
-    private SelectObjectContentResult executeQuery() {
+    private InputStream executeQuery(URI location, String query) {
 
         AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
 
@@ -48,7 +47,6 @@ public class S3SelectQuery extends AbstractEnumerable<Object[]> {
         request.setKey(amazonS3URI.getKey());
         request.setExpression(query);
         request.setExpressionType(ExpressionType.SQL);
-
         CSVInput csvInput = new CSVInput();
         InputSerialization inputSerialization = new InputSerialization();
         inputSerialization.setCsv(csvInput);
@@ -64,6 +62,38 @@ public class S3SelectQuery extends AbstractEnumerable<Object[]> {
         outputSerialization.setCsv(csvOutput);
         request.setOutputSerialization(outputSerialization);
 
-        return s3.selectObjectContent(request);
+        SelectObjectContentResult result = s3.selectObjectContent(request);
+        return result.getPayload().getRecordsInputStream();
     }
+
+
+    @Override
+    public Object[] current() {
+        if (currentLine == null) {
+            throw new NoSuchElementException();
+        }
+        return currentLine;
+    }
+
+    @Override
+    public boolean moveNext() {
+        String[] nextLine = csvParser.parseNext();
+        if (nextLine == null) {
+            return false;
+        } else {
+            currentLine = nextLine;
+            return true;
+        }
+    }
+
+    @Override
+    public void reset() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void close() {
+        csvParser.stopParsing();
+    }
+
 }
--- src/main/java/com/amannmalik/tabulardatatools/calcite/S3Table.java
+++ src/main/java/com/amannmalik/tabulardatatools/calcite/S3Table.java
@@ -1,18 +1,22 @@
 package com.amannmalik.tabulardatatools.calcite;
 
 import org.apache.calcite.DataContext;
+import org.apache.calcite.linq4j.AbstractEnumerable;
 import org.apache.calcite.linq4j.Enumerable;
+import org.apache.calcite.linq4j.Enumerator;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
-import org.apache.calcite.schema.ScannableTable;
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.schema.ProjectableFilterableTable;
 import org.apache.calcite.schema.impl.AbstractTable;
 import org.apache.calcite.sql.type.SqlTypeName;
 
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class S3Table extends AbstractTable implements ScannableTable {
+public class S3Table extends AbstractTable implements ProjectableFilterableTable {
 
     private final URI location;
     private final List<String> fields;
@@ -30,8 +34,17 @@ public class S3Table extends AbstractTable implements ScannableTable {
     }
 
     @Override
-    public Enumerable<Object[]> scan(DataContext root) {
-        return new S3SelectQuery(location, "SELECT * FROM S3Object");
+    public Enumerable<Object[]> scan(DataContext root, List<RexNode> filters, int[] projects) {
+        String selectList = IntStream.of(projects)
+                .boxed()
+                .map(i -> i + 1)
+                .map(i -> "_" + i)
+                .collect(Collectors.joining(", "));
+        return new AbstractEnumerable<>() {
+            @Override
+            public Enumerator<Object[]> enumerator() {
+                return new S3SelectEnumerator(location, String.format("SELECT %s FROM S3Object", selectList));
+            }
+        };
     }
-
 }
